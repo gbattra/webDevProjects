ba4fb41871f20c853b49e5d4c6b5ad8a41b9948d
Gregory Attra <greg.attra@gmail.com>
2015-08-06 09:58:27 -0400

    final function tweeks

--- slidingNewsFeedExample/app.js
+++ slidingNewsFeedExample/app.js
@@ -1,16 +1,17 @@
 var main = function() {
-  $('.article').removeClass('current');
-        $(this).addClass('current');
-        $(this).children('.description').slideToggle();
+  $('.article').click(function() {
+    $('.article').removeClass('current');
+    $('.article').addClass('notCurrent');
+    $(this).removeClass('notCurrent');
+    $(this).addClass('current');
+    $('.notCurrent').children('.description').slideUp();
+    $(this).children('.description').slideToggle();
   });
 
   $(document).keypress(function(event) {
     if (event.which === 111) {
           $('.current').children('.description').slideToggle();
-      };
-    });
-
-
+      }
     else if(event.which === 110) {
       var currentArticle = $('.current');
       var nextArticle = currentArticle.next();
